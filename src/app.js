502b78776ec7c066fe4adf5c6c6bfd5464a1a483
Wendell Ribeiro <wendell.arnald@gmail.com>
2019-03-06 17:12:34 -0300

    Adding a product

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const indexRoute = require('./routes/index-route')
-const productRoute = require('./routes/product-route')
-
 const app = express()
 const router = express.Router()
 
-mongoose.connect('mongodb+srv://<user>:<password>@cluster0-ncn8o.azure.mongodb.net/test?retryWrites=true')
+mongoose.connect('')
+
+const Product = require('./models/product')
+const indexRoute = require('./routes/index-route')
+const productRoute = require('./routes/product-route')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,5 +1,19 @@
+const mongoose = require('mongoose')
+const Product = mongoose.model('Product')
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body)
+    const product = new Product(req.body)
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso!'
+            })
+        }).catch(error => {
+            res.status(400).send({
+                message: 'Falha no cadastro do produto!', data: error
+            })
+        })
 }
 
 exports.put = (req, res, next) => {
