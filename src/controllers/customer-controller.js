57e13af642797b6e28f77399e9d5257c9f8ed4f1
Wendell Ribeiro <wendell.arnald@gmail.com>
2019-08-11 22:27:23 +0100

    Permission for user profile

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -19,7 +19,8 @@ exports.post = async (req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ['user']
         })
 
         emailService.send(
